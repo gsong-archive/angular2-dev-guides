89da723906d2eecc579679bd09c07cd670635b2c
George Song <george@55minutes.com>
2016-02-05 05:55:32 -0800

    Little tour of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,30 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My Second Anuglar 2 App</h1>'
+  template: `
+  <h1>Little Tour of Heroes</h1>
+
+  <input #newHero
+    (keyup.enter)="acceptInput(addHero, newHero)"
+    (blur)="acceptInput(addHero, newHero)">
+  <button (click)="acceptInput(addHero, newHero)">Add</button>
+
+  <ul>
+    <li *ngFor="#hero of heroes">{{ hero }}</li>
+  </ul>
+  `
 })
-export class AppComponent {};
+export class AppComponent {
+  heroes = new Set(['Windstorm', 'Bombasto', 'Magneta', 'Tornado']);
+
+  acceptInput = (callback: (v: string) => void, input: HTMLInputElement) => {
+    callback(input.value);
+    input.value = '';
+  };
+
+  addHero = (hero: string) => {
+    if (hero) {
+      this.heroes = new Set([...Array.from(this.heroes.values()), hero]);
+    }
+  };
+};
